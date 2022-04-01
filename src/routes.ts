0a134d38efbfd01cf2242efa63f10030d5267867
OzRib <ozeiasnascimentoribeiro@gmail.com>
2022-04-01 16:38:14 -0300

    Adding controllers routes in rootRouter

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,12 @@
 import { Router } from 'express';
 import UserRoutes from '@controllers/UserController/routes';
+import PostRoutes from '@controllers/PostController/routes';
+import CommentRoutes from '@controllers/CommentController/routes';
 
 const rootRouter = Router();
 
 rootRouter.use('/users', UserRoutes);
+rootRouter.use('/posts', PostRoutes);
+rootRouter.use('/comments', CommentRoutes);
 
 export default rootRouter;
